f74fa1e038b46455feef34337fb3b59420ab9a1f
Jasper <235980385+jasperusz@users.noreply.github.com>
2026-05-12 15:52:12 -0300

    feat: add randomforestclassifier to predict possible winners

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 # Read CSV variables
 competitions = pd.read_csv('data/competitions.csv')
@@ -34,3 +38,31 @@ choices = ['win', 'loss', 'draw']
 game_results = np.select(conditions, choices, default='unknown')
 merged_games_data['result'] = game_results
 
+# Machine Learning Variables
+X = merged_games_data[['club_id', 'opponent_id', 'hosting', 'home_club_position', 'away_club_position', 'round']].dropna()
+y = merged_games_data['result']
+y = y.loc[X.index]
+
+# Encode categorical variables
+le_hosting = LabelEncoder()
+le_round = LabelEncoder()
+le_result = LabelEncoder()
+
+X['hosting'] = le_hosting.fit_transform(X['hosting'])
+X['round'] = le_round.fit_transform(X['round'])
+y = le_result.fit_transform(y)
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, 
+    test_size=0.2, 
+    random_state=42
+    )
+
+# Train Random Forest Classifier
+model = RandomForestClassifier(n_estimators = 200, random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print('Model accuracy score: {0:0.4f}'. format(accuracy_score(y_test, y_pred)))
